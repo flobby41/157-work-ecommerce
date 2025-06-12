5b6d35f621c8715795845e3b94eaa10d340beef6
flobby41 <florian.cheheb@gmail.com>
2025-06-12 11:43:01 +0200

    feat: update sidebar header icons to display counts with improved layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,25 +17,23 @@ export default function Sidebar({ onCategoryChange, activeCategory }: SidebarPro
          <div className="flex items-center justify-between">
           <div className="flex items-center justify-between w-full text-white">
             {/* Star icon - left */}
-            <div className="flex items-center space-x-1">
-              <span className="text-base">33</span>
+            <div className="relative">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
               </svg>
+              <span className="absolute -bottom-1 -right-1 text-xs bg-transparent">33</span>
             </div>
             
             {/* User icon - middle */}
-            <div className="flex items-center space-x-1">
-              <span className="text-base">5F</span>
+            <div className="relative">
               <User className="w-6 h-6" />
+              <span className="absolute -bottom-1 -right-1 text-xs bg-transparent">JF</span>
             </div>
             
             {/* Shopping cart - right */}
-            <div className="flex items-center space-x-1">
-              <span className="text-base">200</span>
-              <div className="relative">
-                <ShoppingCart className="w-6 h-6" />
-              </div>
+            <div className="relative">
+              <ShoppingCart className="w-6 h-6" />
+              <span className="absolute -bottom-1 -right-1 text-xs bg-transparent">200</span>
             </div>
           </div>
         </div>
